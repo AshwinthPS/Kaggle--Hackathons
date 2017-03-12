a624c8d727fdf79ffe87d63d77ac8259e72ed11c
Ashwinth S <Ashwinth S>
2017-03-12 19:17:56 +0530

    some changes

--- XgBoost.py
+++ XgBoost.py
@@ -14,24 +14,11 @@ os.environ['PATH'] = mingw_path + ';' + os.environ['PATH']
 import xgboost as xgb
 
 data_dir = 'C:\Users\HP\Desktop\Hackathon\March Machine Learning Mania'
-
 df_seeds = pd.read_csv(data_dir + '\TourneySeeds.csv')
-
 df_tour = pd.read_csv(data_dir + '\TourneyCompactResults.csv')
-
 df_tourd=pd.read_csv(data_dir+'\TourneyDetailedResults.csv')
-
-df_tourd.head()
-
 df_sub =pd.read_csv(data_dir+ "\sample_submission.csv")
-
-df_sub
-
-df_tour.head()
-
 df_regd=pd.read_csv(data_dir+'\RegularSeasonDetailedResults.csv')
-
-df_regd.head()
 
 ###################################################  Seeds    ########################################
 
@@ -45,17 +32,10 @@ df_seeds.head()
 df_seeds=df_seeds.drop('Seed',axis=1)
 df_seeds.columns=['Season','team1','Seeds']
 
-df_seeds.head()
-
 df_seeds2=df_seeds.copy()
-df_seeds2.head()
 df_seeds2.columns=['Season','team2','Seeds']
 
-df_seeds2.head()
-
 #########################################################################################################
-
-
 
 final_df=pd.DataFrame()
 final_df[["Season","team1","team2"]] = df_tour[["Season","Wteam","Lteam"]].copy()
@@ -73,35 +53,24 @@ final_df2['seed_dif']=final_df2['Seeds_x']-final_df2['Seeds_y']
 final_df2.head()
 
 final = pd.concat((final_df,final_df2), axis=0)
-final.head()
 final.iloc[2050:2055,:]
 
 final_pred=final[final['Season'] < 2013]
 
-final_pred.head()
-
 pred=final_pred.loc[:,['Season',  'team1'  ,'team2' ,'seed_dif' ]]
 
-
-df_sub.head()
 df_sub.columns=['Season','team1','team2']
 df_sub=pd.merge(df_sub,df_seeds,how='left',on=['Season','team1'])
 df_sub=pd.merge(df_sub,df_seeds2,how='left',on=['Season','team2'])
 df_sub['seed_dif']=df_sub['Seeds_x']-df_sub['Seeds_y']
 df_sub=df_sub.drop(['Seeds_x','Seeds_y'],axis=1)
-df_sub.head()
 
 ###########################################################  MODEL #############################################
-
-
-
 params = {"objective": "binary:logistic","booster": "gbtree", "nthread": 4, "silent": 1,'eval_metric':'logloss',
                 "eta": 0.1, "max_depth": 3, "subsample": 0.9, "colsample_bytree": 0.7, 
                 "min_child_weight": 5,"seed": 2016, "tree_method": "exact"}
 
 params
-
-pred.head()
 
 dtrain = xgb.DMatrix(pred, final_pred.pred, missing=np.nan)
 
@@ -118,8 +87,6 @@ test_preds = bst.predict(dtest)
 test_preds
 
 op1=pd.DataFrame(test_preds)
-
-op1
 
 op1.to_csv("subm.csv")
 
